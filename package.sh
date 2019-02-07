7b95aa03fe53dcd4b932fe260257282bbacbaec5
akinshonibare <akinkunmishonibare@gmail.com>
2019-02-06 22:18:49 -0500

    mvp

--- package.sh
+++ package.sh
@@ -4,4 +4,4 @@ rm -rf dist &&
 mkdir -p dist/client &&
 cp -r server/views server/package.json dist &&
 mv server/server.bundle.js server/server.bundle.js.map dist &&
-cp -r client/build dist/client &&
+cp -r client/build dist/client
